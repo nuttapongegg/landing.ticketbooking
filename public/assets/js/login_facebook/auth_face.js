5dd1c6bd80b9312c5fe55b2ea03d7c7e36553e82
nuttapong <nuttapong.ep@gmail.com>
2022-12-20 14:13:03 +0700

    login

--- public/assets/js/login_facebook/auth_face.js
+++ public/assets/js/login_facebook/auth_face.js
@@ -1,7 +1,7 @@
 window.fbAsyncInit = function() {
     // FB JavaScript SDK configuration and setup
     FB.init({
-      appId      : '1126421014729295', // FB App ID ของพี่มอส(1126421014729295), ของเอ้ก (476847497713912)
+      appId      : '476847497713912', // FB App ID ของพี่มอส(1126421014729295), ของเอ้ก (476847497713912)
       cookie     : true,  // enable cookies to allow the server to access the session
       xfbml      : true,  // parse social plugins on this page
       version    : 'v15.0' // use graph api version 2.8
@@ -60,8 +60,8 @@ function getFbUserData(){
             processData: false,
             data: JSON.stringify(dataObj),
         }),
-        document.getElementById('fbLink').setAttribute("onclick","fbLogout()");
-        document.getElementById('fbLink').innerHTML = '<i class="fa fa-facebook-square" style="font-size:24px" id="'+id+'" aria-hidden="true"></i> Logout Facebook'; // from Facebook
+        document.getElementById('fbLink').setAttribute("onclick","fbLogout()");//<i class="fa fa-facebook-square" style="font-size:24px" aria-hidden="true"></i>
+        document.getElementById('fbLink').innerHTML = '<div class="d-flex"> <div class="me-3 mt-1"> <span class="avatar"> <img src="'+response.picture.data.url+'" alt="img" class="rounded-circle"></span></div><div class="d-flex1 mt-3">&nbsp; <a>Logout Facebook</a></div></div> '; // from Facebook
         // document.getElementById('status').innerHTML = '<p>Thanks for logging in, ' + response.first_name + '!</p>';
         // document.getElementById('userData').innerHTML = '<h2>Facebook Profile Details</h2><p><img src="'+response.picture.data.url+'"/></p><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
     });
