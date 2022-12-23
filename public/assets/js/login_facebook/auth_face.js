b615bc8b15691a66126c77a4eead2664a9697397
Panumas Srisook <Panumas.srisook@gmail.com>
2022-12-23 11:31:11 +0700

    update link sdk.js to all.js

--- public/assets/js/login_facebook/auth_face.js
+++ public/assets/js/login_facebook/auth_face.js
@@ -33,7 +33,7 @@ window.fbAsyncInit = function() {
     var js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/sdk.js";
+    js.src = "https://connect.facebook.net/en_US/all.js";
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));// Facebook login with JavaScript SDK
 
@@ -51,36 +51,36 @@ function fbLogin() {
 
 // Fetch the user profile data from facebook
 function getFbUserData() {
-    // FB.api('/me', { locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture' },
-    //     function (response) {
+    FB.api('/me', { locale: 'en_US', fields: 'id,first_name,last_name,email,link,gender,locale,picture' },
+        function (response) {
 
-    //         let id = response.id
-    //         let name = response.first_name + ' ' + response.last_name
-    //         let email = response.email
-    //         let picture = response.picture.data.url
+            let id = response.id
+            let name = response.first_name + ' ' + response.last_name
+            let email = response.email
+            let picture = response.picture.data.url
 
-    //         let dataObj = {
-    //             id,
-    //             name,
-    //             email,
-    //             picture
-    //         }
-    //         $.ajax({
-    //             type: 'POST',
-    //             url: '/login',
-    //             contentType: 'application/json; charset=utf-8;',
-    //             processData: false,
-    //             data: JSON.stringify(dataObj),
-    //         }),
-    //         document.getElementById('fbLink').setAttribute("onclick", "fbLogout()");
-    //         document.getElementById('fbLink').innerHTML = '<div class="d-flex"> <div class="mt-1"> <span class="avatar"> <img src="' + response.picture.data.url + '" alt="img" class="rounded-circle" style="width: 30px; height: 30px;"></span></div><div class="d-flex1 mt-2">&nbsp; <a>Logout </a></div></div> '; // from Facebook
-    //         // document.getElementById('fbLink').innerHTML = '<i class="fa fa-facebook-square" style="font-size:24px" id="'+id+'" aria-hidden="true"></i> Logout Facebook'; // from Facebook
-    //         // document.getElementById('fbLink').setAttribute("onclick","fbLogout()");//<i class="fa fa-facebook-square" style="font-size:24px" aria-hidden="true"></i>
-    //         // document.getElementById('fbLink').innerHTML = '<div class="d-flex"> <div class="me-3 mt-1"> <span class="avatar"> <img src="'+response.picture.data.url+'" alt="img" class="rounded-circle"></span></div><div class="d-flex1 mt-3">&nbsp; <a>Logout Facebook</a></div></div> '; // from Facebook
-    //         // document.getElementById('status').innerHTML = '<p>Thanks for logging in, ' + response.first_name + '!</p>';
-    //         // document.getElementById('userData').innerHTML = '<h2>Facebook Profile Details</h2><p><img src="'+response.picture.data.url+'"/></p><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
-    //         window.location.reload();
-    //     });
+            let dataObj = {
+                id,
+                name,
+                email,
+                picture
+            }
+            $.ajax({
+                type: 'POST',
+                url: '/login',
+                contentType: 'application/json; charset=utf-8;',
+                processData: false,
+                data: JSON.stringify(dataObj),
+            }),
+            document.getElementById('fbLink').setAttribute("onclick", "fbLogout()");
+            document.getElementById('fbLink').innerHTML = '<div class="d-flex"> <div class="mt-1"> <span class="avatar"> <img src="' + response.picture.data.url + '" alt="img" class="rounded-circle" style="width: 30px; height: 30px;"></span></div><div class="d-flex1 mt-2">&nbsp; <a>Logout </a></div></div> '; // from Facebook
+            // document.getElementById('fbLink').innerHTML = '<i class="fa fa-facebook-square" style="font-size:24px" id="'+id+'" aria-hidden="true"></i> Logout Facebook'; // from Facebook
+            // document.getElementById('fbLink').setAttribute("onclick","fbLogout()");//<i class="fa fa-facebook-square" style="font-size:24px" aria-hidden="true"></i>
+            // document.getElementById('fbLink').innerHTML = '<div class="d-flex"> <div class="me-3 mt-1"> <span class="avatar"> <img src="'+response.picture.data.url+'" alt="img" class="rounded-circle"></span></div><div class="d-flex1 mt-3">&nbsp; <a>Logout Facebook</a></div></div> '; // from Facebook
+            // document.getElementById('status').innerHTML = '<p>Thanks for logging in, ' + response.first_name + '!</p>';
+            // document.getElementById('userData').innerHTML = '<h2>Facebook Profile Details</h2><p><img src="'+response.picture.data.url+'"/></p><p><b>FB ID:</b> '+response.id+'</p><p><b>Name:</b> '+response.first_name+' '+response.last_name+'</p><p><b>Email:</b> '+response.email+'</p><p><b>Gender:</b> '+response.gender+'</p><p><b>FB Profile:</b> <a target="_blank" href="'+response.link+'">click to view profile</a></p>';
+            window.location.reload();
+        });
 }
 
 // Logout from facebook
